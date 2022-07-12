0d36375b2f8a779a2ba5df0038bd491055cec249
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-12 16:54:45 +0900

    감정일기 만들기 감정을 클릭할때마다 단어 옆에 숫자가 올라감 - 지금 구현한 부분은 setEmotionalCounting()함수가 애초에 실행되어 1로 설정되고 클릭할 때마다의 숫자 변경이 반영되지 않음

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet var buttonLabels: [UILabel]!
     @IBOutlet weak var test: UILabel!
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        let emotionWords = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "무력해", "꿀꿇해"]
+        
+        for (i, word) in zip(buttonLabels, emotionWords) {
+            i.text = word + setEmotionCounting()
         
         // 여기에 튜플 써보기
         
@@ -32,45 +37,56 @@ class ViewController: UIViewController {
         //        let colrLiteral = #colorLiteral()
         
     }
+    }
+    
+    @IBAction func clickButton(_ sender: UIButton) {
+        setEmotionCounting()
+    }
+    
+    func setEmotionCounting() -> String {
+        var clickedNumber = 0
+        clickedNumber += 1
+        return String(clickedNumber)
+    }
     
     //배경색, 레이블, 이미지
     func example() -> (UIColor, String, String) {
         let color: [UIColor] = [.yellow, .red, .blue]
         let image: [String] = ["sesac_slime1"]
-            
-            return (color.randomElement()!, "고래밥", image.randomElement()!)
-            }
-            
-            func setUserNickname() {
-                let nickname = ["고래밥", "칙촉", "격투가"]
-                let select = nickname.randomElement()!
-                
-                test.text = select
-            }
-            
-            // 이거 버튼에 넣기
-            // 액션시트에서는 타이틀 및 메세지 잘 않서서 그냥 nil로 둘다 처리
-            func showAlertcontroller() {
-                //1. 흰 바탕: UIAlertController
-                let alert =  UIAlertController(title: "타이틀", message: "여기는 메세지가 들어갑니다.", preferredStyle: .alert)
-                
-                //2.버튼
-                let ok = UIAlertAction(title: "확인", style: .destructive, handler: nil)
-                let cancel = UIAlertAction(title: "취소버튼입니다", style: .cancel, handler: nil)
-                let web = UIAlertAction(title: "새창으로 열기", style: .cancel, handler: nil)
-                let copy = UIAlertAction(title: "복사하기", style: .default, handler: nil)
-                
-                //3. 1+2
-                alert.addAction(ok)
-                alert.addAction(cancel)
-                alert.addAction(web)
-                alert.addAction(copy)
-                
-                
-                //4. present
-                present(alert, animated: true, completion: nil) // completion은 내부적으로 처리하기 위해
-            }
-            
-            
-            }
-            
+        
+        return (color.randomElement()!, "고래밥", image.randomElement()!)
+    }
+    
+    func setUserNickname() {
+        let nickname = ["고래밥", "칙촉", "격투가"]
+        let select = nickname.randomElement()!
+        
+//        test.text = select // 위에 레이블 test 아웃렛 삭제함
+    }
+    
+    // 이거 버튼에 넣기
+    // 액션시트에서는 타이틀 및 메세지 잘 않서서 그냥 nil로 둘다 처리
+    func showAlertcontroller() {
+        //1. 흰 바탕: UIAlertController
+        let alert =  UIAlertController(title: "타이틀", message: "여기는 메세지가 들어갑니다.", preferredStyle: .alert)
+        
+        //2.버튼
+        let ok = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+        let cancel = UIAlertAction(title: "취소버튼입니다", style: .cancel, handler: nil)
+        let web = UIAlertAction(title: "새창으로 열기", style: .cancel, handler: nil)
+        let copy = UIAlertAction(title: "복사하기", style: .default, handler: nil)
+        
+        //3. 1+2
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        alert.addAction(web)
+        alert.addAction(copy)
+        
+        
+        //4. present
+        present(alert, animated: true, completion: nil) // completion은 내부적으로 처리하기 위해
+    }
+    
+    
+}
+
