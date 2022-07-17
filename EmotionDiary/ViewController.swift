f003aebf859ab43eaaa5573a2dd1d5dd3c94d2f8
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-17 11:55:23 +0900

    refactor: 마음비우기 버튼 추가 - 버튼에 여백을 걸어주기 위해 UIButton에 extension을 걸어줌

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
+extension UIButton {
+    func setInsets(forContentPadding contentPadding: UIEdgeInsets, imageTitlePadding: CGFloat?) {
+        
+        if let imageTitlePadding = imageTitlePadding {
+            
+            self.contentEdgeInsets = UIEdgeInsets(
+                top: contentPadding.top,
+                left: contentPadding.left,
+                bottom: contentPadding.bottom,
+                right: contentPadding.right + imageTitlePadding
+            )
+            self.titleEdgeInsets = UIEdgeInsets(
+                top: 0,
+                left: imageTitlePadding,
+                bottom: 0,
+                right: -imageTitlePadding
+            )
+        } else {
+            self.contentEdgeInsets = UIEdgeInsets(
+                top: contentPadding.top,
+                left: contentPadding.left,
+                bottom: contentPadding.bottom,
+                right: contentPadding.right
+            )
+        }
+    }
+}
+
 class ViewController: UIViewController {
     let emotionWords = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "무력해", "꿀꿀해"]
     var emotionArray = [0,0,0,0,0,0,0,0,0]
@@ -14,17 +42,25 @@ class ViewController: UIViewController {
     @IBOutlet var buttonLabels: [UILabel]!
     @IBOutlet var background: UIView!
     @IBOutlet var buttonImage: [UIButton]!
+    @IBOutlet weak var emotionCountReset: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         background.backgroundColor = UIColor.yellow
         
         //MARK: Image 추가 및 tag 추가
-//        inputImage(buttonList: buttonImage)
+        //        inputImage(buttonList: buttonImage)
         inputImage()
         
+        emotionCountReset.setTitle("마음비우기", for: .normal)
+        emotionCountReset.setTitleColor(.black, for: .normal)
+        emotionCountReset.layer.backgroundColor = UIColor.lightGray.cgColor
+        emotionCountReset.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        emotionCountReset.setInsets(forContentPadding: .init(top: 5, left: 5, bottom: 5, right: 5), imageTitlePadding: nil)
+        emotionCountReset.layer.cornerRadius = 10
         
-                
+        
+        
         
         // 이전 실험 코드
         //        for i in 1...9 {
@@ -45,14 +81,14 @@ class ViewController: UIViewController {
         /*
          //MARK: 이거는 내가 작성한 코드
          // 동작: 버튼레이블 행복해등으로 초기화 -> 합성할당된 값 옆에 붙여주기 -> 다음 클릭 -> 다시 초기화
-        buttonLabels[sender.tag].text! = emotionWords[sender.tag]
-        emotionArray[sender.tag] += 1
-        buttonLabels[sender.tag].text! += String(emotionArray[buttonImage[sender.tag].tag])
-        */
+         buttonLabels[sender.tag].text! = emotionWords[sender.tag]
+         emotionArray[sender.tag] += 1
+         buttonLabels[sender.tag].text! += String(emotionArray[buttonImage[sender.tag].tag])
+         */
         
         //MARK: 멘토님이 도와주신 코드..유레카...
-//        emotionArray[sender.tag] += 1
-//        buttonLabels[sender.tag].text = "\(emotionWords[sender.tag])"  + "\(emotionArray[sender.tag])"
+        //        emotionArray[sender.tag] += 1
+        //        buttonLabels[sender.tag].text = "\(emotionWords[sender.tag])"  + "\(emotionArray[sender.tag])"
         
         //MARK: userDefaults 사용해보기
         /*
@@ -70,9 +106,9 @@ class ViewController: UIViewController {
         UserDefaults.standard.set(updateValus, forKey: "emotionClickedCount\([sender.tag])")
         //4.
         buttonLabels[sender.tag].text = "\(emotionWords[sender.tag])\(UserDefaults.standard.integer(forKey: "emotionClickedCount\([sender.tag])"))"
-    
+        
     }
-
+    
     //MARK: 버튼 Image 구성
     func inputImage() {
         for i in 0...8 {
@@ -82,94 +118,94 @@ class ViewController: UIViewController {
             buttonLabels[i].text = "\(emotionWords[i])\(UserDefaults.standard.integer(forKey: "emotionClickedCount\([i])"))"
         }
         /*  -> userDefault를 지정해줘서 쓸 수 없는 코드
-          for (i, word) in zip(0...8, emotionWords) {
-             
-             let image = UIImage(named: "sesac_slime\(i + 1)")
-             buttonImage[i].setImage(image, for: .normal)
-             buttonLabels[i].text = word
+         for (i, word) in zip(0...8, emotionWords) {
+         
+         let image = UIImage(named: "sesac_slime\(i + 1)")
+         buttonImage[i].setImage(image, for: .normal)
+         buttonLabels[i].text = word
          }
- */
-      
+         */
+        
     }
     //
     //        for buttonLabel in buttonLabels {
     //        buttonLabel.text = "\(emotionArray[0])"
     //        }
-
-//MARK: - 수업 및 내 감정일기 코드
-
-//        background.layer.backgroundColor = UIColor.gray.cgColor
-//
-//        let emotionWords = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "무력해", "꿀꿇해"]
-//
-//        for (i, word) in zip(buttonLabels, emotionWords) {
-//            i.text = word
-//
-//        // 여기에 튜플 써보기
-//
-//
-//        // Do any additional setup after loading the view.
-//
-//        // 시스템 심볼 쓰기
-//        // 내 이미지를 쓰려며는 named로 하는 것
-//        //        let systemImage = UIImage(systemName: <#T##String#>)
-//
-//        // 이게 Literal로 이미지 색 넣는 법
-//        // #imageLiteral(
-//        // #imageLiteral(
-//        // 아래코드 주석처리하면 코드가 나옴
-//        //        let literal = #imageLiteral()
-//        //        let colrLiteral = #colorLiteral()
-//    }
-//    }
-//
-//    @IBAction func clickButton(_ sender: UIButton) {
-//        for i in buttonLabels {
-//            i.text = (i.text ?? "감정이 없습니다") + String(setEmotionCounting())
-//        }
-//    }
-//
-//    func setEmotionCounting() -> Int {
-//        var clickedNumber = 0
-//        clickedNumber += 1
-//        return clickedNumber
-//    }
-//
-//    //배경색, 레이블, 이미지
-//    func example() -> (UIColor, String, String) {
-//        let color: [UIColor] = [.yellow, .red, .blue]
-//        let image: [String] = ["sesac_slime1"]
-//
-//        return (color.randomElement()!, "고래밥", image.randomElement()!)
-//    }
-//
-//    func setUserNickname() {
-//        let nickname = ["고래밥", "칙촉", "격투가"]
-//        let select = nickname.randomElement()!
-//
-////        test.text = select // 위에 레이블 test 아웃렛 삭제함
-//    }
-//
-//    // 이거 버튼에 넣기
-//    // 액션시트에서는 타이틀 및 메세지 잘 않서서 그냥 nil로 둘다 처리
-//    func showAlertcontroller() {
-//        //1. 흰 바탕: UIAlertController
-//        let alert =  UIAlertController(title: "타이틀", message: "여기는 메세지가 들어갑니다.", preferredStyle: .alert)
-//
-//        //2.버튼
-//        let ok = UIAlertAction(title: "확인", style: .destructive, handler: nil)
-//        let cancel = UIAlertAction(title: "취소버튼입니다", style: .cancel, handler: nil)
-//        let web = UIAlertAction(title: "새창으로 열기", style: .cancel, handler: nil)
-//        let copy = UIAlertAction(title: "복사하기", style: .default, handler: nil)
-//
-//        //3. 1+2
-//        alert.addAction(ok)
-//        alert.addAction(cancel)
-//        alert.addAction(web)
-//        alert.addAction(copy)
-//
-//
-//        //4. present
-//        present(alert, animated: true, completion: nil) // completion은 내부적으로 처리하기 위해
-//    }
+    
+    //MARK: - 수업 및 내 감정일기 코드
+    
+    //        background.layer.backgroundColor = UIColor.gray.cgColor
+    //
+    //        let emotionWords = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "무력해", "꿀꿇해"]
+    //
+    //        for (i, word) in zip(buttonLabels, emotionWords) {
+    //            i.text = word
+    //
+    //        // 여기에 튜플 써보기
+    //
+    //
+    //        // Do any additional setup after loading the view.
+    //
+    //        // 시스템 심볼 쓰기
+    //        // 내 이미지를 쓰려며는 named로 하는 것
+    //        //        let systemImage = UIImage(systemName: <#T##String#>)
+    //
+    //        // 이게 Literal로 이미지 색 넣는 법
+    //        // #imageLiteral(
+    //        // #imageLiteral(
+    //        // 아래코드 주석처리하면 코드가 나옴
+    //        //        let literal = #imageLiteral()
+    //        //        let colrLiteral = #colorLiteral()
+    //    }
+    //    }
+    //
+    //    @IBAction func clickButton(_ sender: UIButton) {
+    //        for i in buttonLabels {
+    //            i.text = (i.text ?? "감정이 없습니다") + String(setEmotionCounting())
+    //        }
+    //    }
+    //
+    //    func setEmotionCounting() -> Int {
+    //        var clickedNumber = 0
+    //        clickedNumber += 1
+    //        return clickedNumber
+    //    }
+    //
+    //    //배경색, 레이블, 이미지
+    //    func example() -> (UIColor, String, String) {
+    //        let color: [UIColor] = [.yellow, .red, .blue]
+    //        let image: [String] = ["sesac_slime1"]
+    //
+    //        return (color.randomElement()!, "고래밥", image.randomElement()!)
+    //    }
+    //
+    //    func setUserNickname() {
+    //        let nickname = ["고래밥", "칙촉", "격투가"]
+    //        let select = nickname.randomElement()!
+    //
+    ////        test.text = select // 위에 레이블 test 아웃렛 삭제함
+    //    }
+    //
+    //    // 이거 버튼에 넣기
+    //    // 액션시트에서는 타이틀 및 메세지 잘 않서서 그냥 nil로 둘다 처리
+    //    func showAlertcontroller() {
+    //        //1. 흰 바탕: UIAlertController
+    //        let alert =  UIAlertController(title: "타이틀", message: "여기는 메세지가 들어갑니다.", preferredStyle: .alert)
+    //
+    //        //2.버튼
+    //        let ok = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+    //        let cancel = UIAlertAction(title: "취소버튼입니다", style: .cancel, handler: nil)
+    //        let web = UIAlertAction(title: "새창으로 열기", style: .cancel, handler: nil)
+    //        let copy = UIAlertAction(title: "복사하기", style: .default, handler: nil)
+    //
+    //        //3. 1+2
+    //        alert.addAction(ok)
+    //        alert.addAction(cancel)
+    //        alert.addAction(web)
+    //        alert.addAction(copy)
+    //
+    //
+    //        //4. present
+    //        present(alert, animated: true, completion: nil) // completion은 내부적으로 처리하기 위해
+    //    }
 }
